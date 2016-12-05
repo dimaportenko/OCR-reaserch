a3a0294d64979b40813fdf0d6fcb3aed8b750a50
Dmytro Portenko <dvportenko@gmail.com>
2016-12-06 00:01:33 +0200

    update container model

---
+++ app/src/main/java/com/google/android/gms/samples/vision/ocrreader/data/ContainerModel.java
@@ -0,0 +1,24 @@
+package com.google.android.gms.samples.vision.ocrreader.data;
+
+import com.google.android.gms.samples.vision.ocrreader.helper.UniqID;
+
+/**
+ * Created by Dmytro Portenko on 11/29/16.
+ */
+
+public class ContainerModel {
+
+    private int mId;
+
+    ContainerModel() {
+        this(UniqID.generateID());
+    }
+
+    ContainerModel(int id) {
+        mId = id;
+    }
+
+    public int getId() {
+        return mId;
+    }
+}
