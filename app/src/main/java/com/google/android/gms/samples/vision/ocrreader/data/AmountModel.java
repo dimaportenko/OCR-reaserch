f4fff4d19133ccb2ba10f0f41571b8d629238a22
Dmytro <troublesohard@gmail.com>
2017-05-25 00:29:44 +0300

    add amount model

---
+++ app/src/main/java/com/google/android/gms/samples/vision/ocrreader/data/AmountModel.java
@@ -0,0 +1,10 @@
+package com.google.android.gms.samples.vision.ocrreader.data;
+
+/**
+ * Created by Dmytro Portenko on 5/25/17.
+ */
+
+public class AmountModel {
+    public int amount = 1;
+    public int total = 1;
+}
--- app/src/main/java/com/google/android/gms/samples/vision/ocrreader/data/ContainerModel.java
+++ app/src/main/java/com/google/android/gms/samples/vision/ocrreader/data/ContainerModel.java
@@ -1,6 +1,9 @@
 package com.google.android.gms.samples.vision.ocrreader.data;
 
 import com.google.android.gms.samples.vision.ocrreader.helper.UniqID;
+import com.google.android.gms.vision.text.Text;
+
+import java.util.HashMap;
 
 /**
  * Created by Dmytro Portenko on 11/29/16.
@@ -9,6 +12,7 @@ import com.google.android.gms.samples.vision.ocrreader.helper.UniqID;
 public class ContainerModel {
 
     private int mId;
+    private HashMap<Text, AmountModel> mTextAmounts;
 
     ContainerModel() {
         this(UniqID.generateID());
@@ -22,22 +26,22 @@ public class ContainerModel {
         return mId;
     }
 
-    private int mAmount = 1;
-    private int mTotalAmount = 1;
-
-    public int getmAmount() {
-        return mAmount;
-    }
-
-    public void setmAmount(int mAmount) {
-        this.mAmount = mAmount;
-    }
-
-    public int getmTotalAmount() {
-        return mTotalAmount;
-    }
-
-    public void setmTotalAmount(int mTotalAmount) {
-        this.mTotalAmount = mTotalAmount;
-    }
+//    private int mAmount = 1;
+//    private int mTotalAmount = 1;
+//
+//    public int getmAmount() {
+//        return mAmount;
+//    }
+//
+//    public void setmAmount(int mAmount) {
+//        this.mAmount = mAmount;
+//    }
+//
+//    public int getmTotalAmount() {
+//        return mTotalAmount;
+//    }
+//
+//    public void setmTotalAmount(int mTotalAmount) {
+//        this.mTotalAmount = mTotalAmount;
+//    }
 }
